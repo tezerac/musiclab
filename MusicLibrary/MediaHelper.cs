141b707f4aa3119de656d3d2afea02913a921c2c
tezerac <41699924+tezerac@users.noreply.github.com>
2018-09-03 22:24:55 -0700

    check it

---
+++ MusicLibrary/MediaHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Storage.FileProperties;
+
+namespace MusicLibrary
+{
+    public static class MediaHelper
+    {
+        public static async Task <string > GetMediaFileAsync(string filename)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var mediaFile = await localFolder.GetFileAsync(filename);
+            var mediaStream = await mediaFile.OpenReadAsync();
+            var mediaReader = new DataReader(mediaStream );
+            var mediaLength = mediaStream.Size;
+            await mediaReader.LoadAsync((uint)mediaLength );
+            return mediaReader.ReadString((uint)mediaLength);
+        }
+        public static async void WriteMediaFileAsync(string filename,string content)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var mediaFile = await localFolder.CreateFileAsync(filename,CreationCollisionOption.OpenIfExists);
+            var mediaStream = await mediaFile.OpenAsync(FileAccessMode.ReadWrite);
+            var mediaWriter = new DataWriter(mediaStream);
+            mediaWriter.WriteString(content);
+            await mediaWriter.StoreAsync();
+            
+        }
+    }
+}
---
+++ MusicLibrary/MusicClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary
+{
+    public class MusicClass
+    {
+
+        private const string MEDIA_FILE_NAME = "MultiMediaFile.mp4";
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Length { get; set; }
+        public string Genre { get; set; }
+
+        public static async Task<ICollection<MusicClass>> GetMusic()
+        {
+            var musicClasses = new List<MusicClass>();
+            var fileContent = await MediaHelper.GetMediaFileAsync(MEDIA_FILE_NAME);
+            var lines = fileContent.Split(new char[] { '\r', '\n' });
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+                var linesPart = line.Split(',');
+                var musicClass = new MusicClass
+                {
+
+                    Title = linesPart[0],
+                    Artist = linesPart[1],
+                    Album = linesPart[2],
+                    Length = linesPart[3],
+                    Genre = linesPart[4]
+                };
+                musicClasses.Add(musicClass);
+            }
+            return musicClasses;
+        }
+
+
+        public static void AddMusic(MusicClass musicClass)
+        {
+            var musicData = $"{musicClass.Title },{musicClass.Artist},{musicClass.Album },{musicClass.Length },{musicClass.Genre }";
+            MediaHelper.WriteMediaFileAsync(MEDIA_FILE_NAME, musicData);
+        }
+
+
+    }
+}
+
